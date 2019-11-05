3de88da146ad7cb2c880b5fd5bb7878286388c1a
Florian Meyer <florian.meyer@capgemini.com>
2019-11-05 08:18:36 +0100

    Add project files.

---
+++ JarvisModuleCore/Attributes/JarvisModuleAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JarvisModuleCore.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class JarvisModuleAttribute : Attribute
+    {
+        public string Name { get; }
+        public JarvisModuleAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
